e9ea37f90c381b0f5df8e5fa1dbd95b24beb0b38
Leak <sethpowellbusiness@gmail.com>
2021-08-19 10:24:37 -0700

    added fetch request for books

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,20 @@ import BooksWrapper from "./pages/books-wrapper"
 import AddBook from "./pages/add-book"
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      bookdata: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/book/get")
+    .then(response => response.json())
+    .then(data => this.setState({bookdata: data}))
+  }
+
   render() {
     return (
       <BrowserRouter>
